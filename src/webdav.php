dc63b2279d9bc616ae0224f72d1d62eb7d61adbb
lliurex <fercavi666@gmail.com>
2016-07-18 12:59:07 +0200

    Initial

--- src/webdav.php
+++ src/webdav.php
@@ -4,14 +4,58 @@ class webdav{
 	private $server;
 	private $user;
 	private $pass;
-	private $urlMoodle;
+	private $url;
+	private $handle;
+
 	public function __construct($server){
 		$this->server = $server;
-		$this->urlMoodle = $server."remote.php/webdav/";
+		$this->url = $server."remote.php/webdav/";
 	}
-	public function getUrlMoodle(){
-		return $this->urlMoodle;
+	public function getUrl(){
+		return $this->url;
 	}
+	public function connect($user,$pass){
+		$this->handle = curl_init();
+		$this->user = $user;
+		$this->pass = $pass;
+	}
+	public function getFolder(){
+		curl_setopt($this->handle, CURLOPT_URL, $this->url);
+		curl_setopt($this->handle, CURLOPT_HEADER, 0);
+		curl_setopt($this->handle, CURLOPT_USERPWD, $this->user.":".$this->pass);
+		curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, 'PROPFIND');
+		$data=curl_exec($this->handle);			
+		return($data);
+	}
+	public function getFile($path){
+		curl_setopt($this->handle, CURLOPT_URL, $this->url.$path);
+		curl_setopt($this->handle, CURLOPT_HEADER, 0);
+		curl_setopt($this->handle, CURLOPT_USERPWD, $this->user.":".$this->pass);
+		curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, 'GET');		
+		$data=curl_exec($this->handle);		
+		return($data);
+	}
+	public function getHandle(){
+		return $this->handle;
+	}
+	public function putFile($source,$target){
+
+		curl_setopt($this->handle, CURLOPT_URL, $this->url.$target);
+		curl_setopt($this->handle, CURLOPT_HEADER, 0);
+		curl_setopt($this->handle, CURLOPT_USERPWD, $this->user.":".$this->pass);		
+		curl_setopt($this->handle, CURLOPT_RETURNTRANSFER, true);	
+		curl_setopt($this->handle, CURLOPT_PUT,1);
+		curl_setopt($this->handle, CURLOPT_CUSTOMREQUEST, 'PUT');
+		$fh_res = fopen($source, 'r');
+		curl_setopt($this->handle, CURLOPT_INFILE, $fh_res);
+		curl_setopt($this->handle, CURLOPT_INFILESIZE, filesize($source));		
+		$data=curl_exec($this->handle);			
+		return($data);	
+
+	}
+
 }
 	
 
--- tests/webdavTest.php
+++ tests/webdavTest.php
@@ -5,8 +5,32 @@ class webdavTest extends TestCase{
 	public function testgetUrl(){
 		$server ="owncloud.localhost.com/";
 		$WD = new webdav($server);
-		$this->assertEquals($WD->getUrlMoodle(),"owncloud.localhost.com/remote.php/webdav/");
+		$this->assertEquals($WD->getUrl(),"owncloud.localhost.com/remote.php/webdav/");
 		
+	}
+	public function testGetFolder(){
+		$server ="owncloud.localhost.com/";
+		$WD = new webdav($server);
+		$WD->connect("admin","lliurex");
+		$folder = $WD->getFolder();
+		$this->assertStringStartsWith("<?xml version",$folder);
+	}
+	public function testGetFile(){
+		$server ="owncloud.localhost.com/";
+		$WD = new webdav($server);
+		$WD->connect("admin","lliurex");
+		$file = $WD->getFile("/test.xml");//
+		$this->assertStringStartsWith("<?xml version",$file);				
+	}
+	public function testPutFile(){
+		$server ="owncloud.localhost.com/";
+		$WD = new webdav($server);
+		$WD->connect("admin","lliurex");
+		$source = "/tmp/p.txt";
+		$target = "/p.txt";
+		$file = $WD->putFile($source,$target);
+		
+		//$this->assertStringStartsWith("<?xml version",$file);					
 	}
 }
 
